59a197a85e0b191841ad18709e8db8812398ad7e
murrayw717rm-lgtm <murrayw717rm@gmail.com>
2025-11-25 13:59:06 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,17 +1,15 @@
-// 題目 1：高塔建築師 (The Tower Architect) - 20分
-
 #include <iostream>
 using namespace std;
 
 long long factorial(int n) {
-    if (n == 1)
+    if (n == 1 || n == 0)
         return 1;
     return n * factorial(n - 1);
 }
 
 long long sum(int n) {
-    if (n == 1)
-        return 1;
+    if (n == 0)
+        return 0;
     return n + sum(n - 1);
 }
 
