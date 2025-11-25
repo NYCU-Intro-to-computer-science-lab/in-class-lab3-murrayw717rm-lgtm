ef577f851204e165bf450dd3e13e22ff9e22b2e1
murrayw717rm-lgtm <murrayw717rm@gmail.com>
2025-11-25 14:13:47 +0800

    Update p4.cpp

--- src/p4.cpp
+++ src/p4.cpp
@@ -1,10 +1,33 @@
-// 題目 4：遞迴的足跡 (Footsteps of Recursion) - 30分
-
 #include <iostream>
 using namespace std;
 
+void printIndent(int depth) {
+    if (depth <= 0) return;
+    cout << "|--";
+    printIndent(depth - 1);
+}
+
+
+int fib(int n, int depth) {
+
+    if (depth > 0) printIndent(depth);
+    cout << "SEARCH fib(" << n << ")\n";
+    int result;
+    if (n == 1 || n == 2) {
+        result = 1;
+    } else {
+        int a = fib(n - 1, depth + 1);
+        int b = fib(n - 2, depth + 1);
+        result = a + b;
+    }
+    if (depth > 0) printIndent(depth);
+    cout << "GET fib(" << n << ") = " << result << "\n";
+    return result;
+}
+
 int main() {
-    
-    
+    int n;
+    cin >> n;
+    cout << fib(n, 0) << endl;
     return 0;
 }
